0734b8361a6f7c29534669ac923052e0cb561666
Unknown <guillem.lion.benosa@gmail.com>
2018-06-08 18:28:19 +0200

    update

---
+++ installDocker.sh
@@ -0,0 +1,18 @@
+# 2.) Install Docker CE for Ubuntu by following their official documentation.
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+# 3.) Install Docker Compose for Linux by following their official documentation.
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
