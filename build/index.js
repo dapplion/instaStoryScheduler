87c6fb1576f24fd5a029731a0a65a8e57bcd11a6
Unknown <guillem.lion.benosa@gmail.com>
2018-06-08 19:48:29 +0200

    create cookies folder

--- build/index.js
+++ build/index.js
@@ -18,6 +18,9 @@ if (!process.env.EMAILUSER) throw Error('Undefined process env: EMAILUSER')
 if (!process.env.EMAILPASS) throw Error('Undefined process env: EMAILPASS')
 if (!process.env.EMAILTO) throw Error('No receiver specified')
 
+// Initialize cookies folder
+fs.mkdirSync('./modules/cookies')
+
 
 const credentials = {
   username: USER,
